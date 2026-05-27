ff2ff454b5b07005e1524a8a58a5aaec21834139
umayasalwaputri-cpu <umayasalwaputri@gmail.com>
2026-05-27 09:55:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,4 +79,67 @@ if menu == "Titrasi":
         st.success("Titik ekuivalen tercapai 🎉")
 
     else:
+import streamlit as st
+import time
+
+st.title("🧪 VirtualChem Lab")
+
+st.header("⚖️ Simulasi Penimbangan")
+
+# gambar neraca
+st.image(
+    "https://cdn-icons-png.flaticon.com/512/2921/2921822.png",
+    width=250
+)
+
+st.write("Tambahkan bahan kimia ke neraca")
+
+# slider massa
+massa = st.slider(
+    "Geser untuk menambahkan massa",
+    0.0,
+    10.0,
+    0.0,
+    0.1
+)
+
+# animasi loading penimbangan
+if st.button("Mulai Menimbang"):
+
+    progress = st.progress(0)
+
+    for i in range(100):
+        time.sleep(0.01)
+        progress.progress(i + 1)
+
+    st.success(
+        f"Penimbangan selesai: {massa} gram"
+    )
+
+    # simulasi hasil di layar neraca
+    st.metric(
+        label="Hasil Neraca Digital",
+        value=f"{massa} g"
+    )
+
+    st.balloons()
+volume = st.slider(
+    "Putar buret (mL)",
+    0,
+    50
+)
+
+if volume < 25:
+    st.info("Larutan masih bening")
+
+elif volume == 25:
+    st.success("Titik ekuivalen tercapai")
+
+else:
+    st.warning("Larutan berubah pink")
+
+    
+
+
+        
         st.warning("Larutan berubah menjadi pink")
