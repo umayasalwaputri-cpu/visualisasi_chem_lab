85778e1129f4c8e2228692e0872829e6b32c2552
umayasalwaputri-cpu <umayasalwaputri@gmail.com>
2026-05-27 09:46:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.title("🧪 VirtualChem Lab")
 
@@ -9,44 +10,73 @@ menu = st.selectbox(
     ["Menimbang", "Titrasi"]
 )
 
-# Simulasi Menimbang
+# =========================
+# SIMULASI MENIMBANG
+# =========================
 if menu == "Menimbang":
 
-    st.header("Simulasi Menimbang")
+    st.header("⚖️ Simulasi Menimbang")
 
-    massa = st.number_input(
-        "Masukkan massa (gram)",
-        min_value=0.0
+    # Pilihan bahan
+    bahan = st.selectbox(
+        "Pilih Bahan Kimia",
+        ["NaOH", "HCl", "KCl", "CuSO4"]
     )
 
-    if st.button("Timbang"):
-        st.success(
-            f"Berhasil menimbang {massa} gram"
+    # Target massa
+    target = st.number_input(
+        "Target Massa (gram)",
+        min_value=0.0,
+        step=0.01
+    )
+
+    st.image(
+        "https://cdn-icons-png.flaticon.com/512/2921/2921822.png",
+        width=150
+    )
+
+    if st.button("Mulai Menimbang"):
+
+        # simulasi hasil massa
+        hasil = round(
+            random.uniform(target - 0.02, target + 0.02),
+            2
         )
 
-# Simulasi Titrasi
+        st.subheader("Hasil Penimbangan")
+
+        st.success(
+            f"{bahan} berhasil ditimbang {hasil} gram"
+        )
+
+        # pengecekan akurasi
+        if hasil == target:
+            st.info("Penimbangan sangat akurat ✅")
+
+        elif hasil > target:
+            st.warning("Massa melebihi target ⚠️")
+
+        else:
+            st.warning("Massa kurang dari target ⚠️")
+
+# =========================
+# SIMULASI TITRASI
+# =========================
 if menu == "Titrasi":
 
-    st.header("Simulasi Titrasi")
+    st.header("🧪 Simulasi Titrasi")
 
-    if st.button("Mulai Titrasi"):
-        st.info(
-            "Larutan berubah warna menjadi pink"
-        )
-# Judul
-st.title("Simulasi Menimbang")
+    volume = st.slider(
+        "Tambahkan Volume NaOH (mL)",
+        0,
+        50
+    )
 
-# Input massa
-massa = st.number_input(
-    "Masukkan massa (gram)",
-    min_value=0.0,
-    step=0.1
-)
+    if volume < 25:
+        st.info("Larutan masih bening")
 
-# Tombol
-if st.button("Timbang"):
-    st.success(f"Hasil penimbangan: {massa} gram")
+    elif volume == 25:
+        st.success("Titik ekuivalen tercapai 🎉")
 
-
-    
-        
+    else:
+        st.warning("Larutan berubah menjadi pink")
